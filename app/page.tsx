ade99482743273a06cfb6f37446ff18b320d6022
useleadnest <useleadnest@gmail.com>
2025-08-15 21:55:21 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>LeadNest</h1>
+      <p>Welcome! <a href="/signup">Start your 7-day trial</a></p>
+    </main>
+  );
+}
